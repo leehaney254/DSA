63d9e0b58bf075e404a638521f0bf9785dfd7a4b
Leehaney gee <georgeleehaney20@gmail.com>
2023-09-03 11:56:22 +0300

    Solve binary

---
+++ binary.js
@@ -0,0 +1,28 @@
+var addBinary = function (a, b) {
+  let carry = 0;
+  let result = "";
+  let i = a.length - 1;
+  let j = b.length - 1;
+
+  while (i >= 0 || j >= 0) {
+    let sum = carry;
+
+    if (a[i]) {
+      sum += parseInt(a[i]);
+      i--;
+    }
+
+    if (b[j]) {
+      sum += parseInt(b[j]);
+      j--;
+    }
+
+    carry = sum > 1 ? 1 : 0;
+    result += sum % 2;
+  }
+
+  if (carry === 1) result += carry;
+  return result.split("").reverse().join("");
+};
+
+console.log(addBinary("11", "1"));
